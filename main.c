305224b5658da06757d11f8688513398bc047533
kentlincku <kurt349761@gmail.com>
2019-03-03 17:13:57 +0800

    update

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*func[])(int a, int b)={plus, minus, multiply, divided};
 int main()
 {
 	int a, c;
@@ -13,11 +14,12 @@ int main()
 	scanf("%i %c %i", &a, &b, &c);
 	//IF ELSE IF
 	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
+		printf("%d %c %d = %d\n", a, b, c, func[0](a, c));
 	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
+		printf("%d %c %d = %d\n", a, b, c, func[1](a, c));
 	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
+		printf("%d %c %d = %d\n", a, b, c, func[2](a, c));
 	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+		printf("%d %c %d = %d\n", a, b, c, func[3](a, c));
+
 }
